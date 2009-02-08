1b14cb5048639aa48e2c62163ded9a0edcd3ce09
Fabio Mattei <fabio@nemo.local>
2009-02-08 00:17:36 +0000

    sistemata file configurazione

--- config.php
+++ config.php
@@ -19,24 +19,18 @@
 
 
 // ** MySQL settings ** //
-define('DB_NAME', 'wordpress');    // The name of the database
+define('DB_NAME', 'easymagazine');    // The name of the database
 define('DB_USER', 'root');     // Your MySQL username
 define('DB_PASSWORD', 'root'); // ...and password
-define('DB_HOST', 'localhost');    // 99% chance you won't need to change this value
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
+define('DB_HOST', 'localhost:8889');    // 99% chance you won't need to change this value
+//  define('DB_CHARSET', 'utf8');
 
 // You can have multiple installations in one database if you give each a unique prefix
 $table_prefix  = 'em_';   // Only numbers, letters, and underscores please!
 
-// Change this to localize WordPress.  A corresponding MO file for the
-// chosen language must be installed to wp-content/languages.
-// For example, install de.mo to wp-content/languages and set WPLANG to 'de'
-// to enable German language support.
-define ('WPLANG', 'IT');
+// Change this to localize Easy Magazine.
+// A corresponding file for the
+// chosen language must be installed to contents/languages.
+define ('EMLANG', 'IT');
 
-/* That's all, stop editing! Happy blogging. */
-
-define('ABSPATH', dirname(__FILE__).'/');
-require_once(ABSPATH.'wp-settings.php');
 ?>
