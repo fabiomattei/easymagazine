fec3aefc8e6506d696ab1fbb1bd253d718dcdfbd
Fabio Mattei <burattino@gmail.com>
2009-03-24 22:52:59 +0000

    created DBCreator class specialized in db creation

--- test/admin/datamodel/tarticle.php
+++ test/admin/datamodel/tarticle.php
@@ -56,61 +56,9 @@ class ArticleTests extends UnitTestCase {
 		$this->assertPattern('(Titolo)', $out);
     }
 
-   function TestNumberInCompleteQuery() {
-	    $str = 'insert into articles (id, number_id, indexnumber, published, title, subtitle, summary, body, tag, metadescription, metakeyword, created, updated) values (#, #, #, #, ?, ?, ?, ?, ?, ?, ?, now(), now())';
-	    $array_strings = array("Titolo", "Sottotitolo", "sommario", "body", "TAg1 tag 2", "Metadescription", "Metakeyword");
-        $array_int = array(1, 2, 3, 1);
-        $tables = array("articles" => "Pretab"."articles");
-	    $out = StrHelper::formatQRY($str, $array_strings, $array_int ,$tables);
-		$this->assertPattern('(1)', $out);
-    }
-
-   function TestTableNameInCompleteQuery() {
-	    $str = 'insert into articles (id, number_id, indexnumber, published, title, subtitle, summary, body, tag, metadescription, metakeyword, created, updated) values (#, #, #, #, ?, ?, ?, ?, ?, ?, ?, now(), now())';
-	    $array_strings = array("Titolo", "Sottotitolo", "sommario", "body", "TAg1 tag 2", "Metadescription", "Metakeyword");
-        $array_int = array(1, 2, 3, 1);
-        $tables = array("articles" => "Pretab"."articles");
-	    $out = StrHelper::formatQRY($str, $array_strings, $array_int ,$tables);
-		$this->assertPattern('(Pretabarticles)', $out);
-    }
-
-   function TestReplaceTitoloString() {
-	    $str = 'insert into articles (id, number_id, indexnumber, published, title, subtitle, summary, body, tag, metadescription, metakeyword, created, updated) values (#, #, #, #, ?, ?, ?, ?, ?, ?, ?, now(), now())';
-	    $array = array("Titolo", "Sottotitolo", "sommario", "body", "TAg1 tag 2", "Metadescription", "Metakeyword");
-	    $out = StrHelper::replaceStrings($str, $array);
-		$this->assertPattern('(Titolo)', $out);
-    }
-
-   function TestReplaceMetakeywordString() {
-	    $str = 'insert into articles (id, number_id, indexnumber, published, title, subtitle, summary, body, tag, metadescription, metakeyword, created, updated) values (#, #, #, #, ?, ?, ?, ?, ?, ?, ?, now(), now())';
-	    $array = array("Titolo", "Sottotitolo", "sommario", "body", "TAg1 tag 2", "Metadescription", "Metakeyword");
-	    $out = StrHelper::replaceStrings($str, $array);
-		$this->assertPattern('(Metakeyword)', $out);
-    }
-
-    function TestReplaceIdNumber() {
-	    $str = 'insert into articles (id, number_id, indexnumber, published, title, subtitle, summary, body, tag, metadescription, metakeyword, created, updated) values (#, #, #, #, ?, ?, ?, ?, ?, ?, ?, now(), now())';
-	    $array = array(1, 2, 3, 4);
-	    $out = StrHelper::replaceNumbers($str, $array);
-		$this->assertPattern('(1)', $out);
-    }
-
-    function TestReplaceLastNumber() {
-	    $str = 'insert into articles (id, number_id, indexnumber, published, title, subtitle, summary, body, tag, metadescription, metakeyword, created, updated) values (#, #, #, #, ?, ?, ?, ?, ?, ?, ?, now(), now())';
-	    $array = array(1, 2, 3, 4);
-	    $out = StrHelper::replaceNumbers($str, $array);
-		$this->assertPattern('(4)', $out);
-    }
-
-    function TestReplaceTableName() {
-	    $str = 'insert into articles (id, number_id, indexnumber, published, title, subtitle, summary, body, tag, metadescription, metakeyword, created, updated) values (#, #, #, #, ?, ?, ?, ?, ?, ?, ?, now(), now())';
-	    $tables = array("articles" => "Pretab"."articles");
-	    $out = StrHelper::fixTableName($str, $tables);
-		$this->assertPattern('(Pretabarticles)', $out);
-    }
 }
 
-$test = new StrHelperTests();
+$test = new ArticleTests();
 $test->run(new HtmlReporter());
 
 ?>
