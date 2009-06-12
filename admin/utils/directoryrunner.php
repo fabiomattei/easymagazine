88406718f0752a17af02d34f88172fa4fa2f23cb
Fabio Mattei <burattino@gmail.com>
2009-06-12 13:04:23 +0100

    added file runner

---
+++ admin/utils/directoryrunner.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+    Copyright (C) 2009  Fabio Mattei
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class DirectoryRunner {
+
+    public static function retrivePlugInList() {
+        $list = array();
+        $dir = STARTPATH.'contents/plug_in/';
+
+        if (is_dir($dir)) {
+            $dh = opendir($dir);
+            if ($dh) {
+                while (($file = readdir($dh)) !== false) {
+                    if (filetype($dir.$file)=='dir') {
+                        $list["$file"] = $file;
+                    }
+                }
+                closedir($dh);
+            }
+        }
+        return $list;
+    }
+
+}
+
+?>
