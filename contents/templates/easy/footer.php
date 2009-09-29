b22e5176b513b38bfefb43b43b0358b2d1203943
Fabio Mattei <burattino@gmail.com>
2009-09-29 15:14:28 +0100

    fixed footer fro easy template

--- contents/templates/easy/footer.php
+++ contents/templates/easy/footer.php
@@ -1,1 +1,16 @@
+</div>
+
+<!-- The main column ends  -->
+
+<!-- begin footer -->
+
+<div style="clear:both;"></div>
+<div style="clear:both;"></div>
+</div>
+
+<div id="footer">
 <?= Magazine::getMagazinePublisher()?> <?= Magazine::getMagazineRights()?> Powered by <a href="http://www.easymagazine.org/">Easy Magazine</a>
+</div>
+</body>
+
+</html>
