51011ddee8085c38ae0ae878f18ed4e0726d2f23
Fabio Mattei <burattino@gmail.com>
2010-02-11 17:39:36 +0000

    changed copyright

--- contents/plug_in/linkmanager/index.php
+++ contents/plug_in/linkmanager/index.php
@@ -15,37 +15,27 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+*/
 
 require_once(STARTPATH.DBPATH.'db.php');
 
-$tables = array("links" => TBPREFIX."links");
-$SQL = 'CREATE TABLE '.TBPREFIX.'links (
-           id int(11) NOT NULL auto_increment,
-           title varchar(255),
-           text varchar(255),
-           url text,
-           PRIMARY KEY (id));';
-$array_str = array();
-$array_int = array();
+class LinkPlugIn{
 
-$rs = DB::getInstance()->execute(
-    $SQL,
-    $array_str,
-    $array_int,
-    $tables);
+    public static function getLinkList() {
+        $tables = array("links" => TBPREFIX."links");
+        $SQL = 'SELECT * FROM links ';
+        $array_str = array();
+        $array_int = array();
 
+        $rs = DB::getInstance()->execute(
+                $SQL,
+                $array_str,
+                $array_int,
+                $tables);
 
-$SQL = "insert into ".TBPREFIX."links (id, title, text, url)
-        values (0, 'My link', 'My link', 'http://www.easymagazine.org')";
+        return $rs;
+    }
 
-$rs = DB::getInstance()->execute(
-    $SQL,
-    $array_str,
-    $array_int,
-    $tables);
-
-echo 'Database activated';
-
+}
 
 ?>
