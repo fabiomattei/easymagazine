b13b70521c3f32ca64c6f0bb20aca1d55b84439e
Fabio Mattei <burattino@gmail.com>
2010-03-20 10:59:59 +0000

    updating link plugin information file

--- contents/plug_in/linkmanager/info.php
+++ contents/plug_in/linkmanager/info.php
@@ -6,12 +6,14 @@
         <td class="first" width="172">
             The "link manager" plug-in is a working example of administration of a links
             table.<br />
-            The administration is complete a show an example of all CRUD operation.
-            In order to use it you need to insert code like this somewhere in your template
-            script:<br />
+            The administration is complete a show an example of all CRUD operation.<br />
+            In order to let your readers enjoy your links list, you need to insert, in your template
+            script, the following code:<br /><br />
             while ($row = mysql_fetch_array(LinkPlugIn::getLinkList())) {<br />
-            &nbsp;&nbsp;&nbsp;&nbsp;echo $row['id'].' - '.$row['title'].' - '.$row['text'].' - '.$row['url'].' |
+            &nbsp;&nbsp;&nbsp;&nbsp;echo $row['id'].' - '.$row['title'].' - '.$row['text'].' - '.$row['url']; <br />
             }<br />
+            <br />
+            Don't foget to activate the plugin after you put the code in your template script.
         </td>
     </tr>
 </table>
