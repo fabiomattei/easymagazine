5ff5b7c32f632157a718170a432d881ef510abbf
Fabio Mattei <burattino@gmail.com>
2010-05-09 12:55:35 +0100

    starting to test the administration interface

---
+++ test/testinterface.php
@@ -0,0 +1,58 @@
+<?php
+/*
+    Copyright (C) 2009-2010  Fabio Mattei <burattino@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+define('BASEDIR', '../');
+define('OUTDIR', BASEDIR.'../');
+define('STARTPATH', '../');
+
+require_once(OUTDIR.'simpletest/web_tester.php');
+require_once(OUTDIR.'simpletest/autorun.php');
+
+//require_once('dummyDBCreator.php');
+
+/* Repopulates the schema in order to restart to develop */
+/*
+$db = new DbCreator();
+$db->connect();
+$db->dropSchema();
+$db->createSchema();
+$db->populateSchema();
+ */
+
+class AllAcceptanceTests extends TestSuite {
+
+    
+    function __construct() {
+        parent::__construct('All Acceptance Tests');
+        $this->addFile('admin/view/publisher/numberst.php');
+        $this->addFile('admin/view/publisher/articlest.php');
+    }
+
+    /*
+    function AllTests() {
+        $this->TestSuite('All tests for SimpleTest ' . SimpleTest::getVersion());
+        $this->addFile('admin/view/publisher/numberst.php');
+        $this->addFile('admin/view/publisher/articlest.php');
+    }*/
+
+}
+
+$test = new AllAcceptanceTests();
+$test->run(new HtmlReporter());
+
+?>
