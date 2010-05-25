dc0513eba10a88608d3d5335a6a0b59ca90979eb
Fabio Mattei <burattino@gmail.com>
2010-05-25 17:26:10 +0100

    implemento supporto ai plugin nei template di default

---
+++ contents/templates/easy/plugin.php
@@ -0,0 +1,13 @@
+<div id="content">
+
+    <?php include("l_sidebar.php");?>
+
+    <div id="contentmiddle">
+
+        <?php include('contents/plug_in/'.$this->arURI['plugin'].'/'.$this->arURI['script'].'.php');?>
+
+    </div>
+
+    <?php include("r_sidebar.php");?>
+
+</div>
