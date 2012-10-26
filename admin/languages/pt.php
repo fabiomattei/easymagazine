98cd44f54a3857937b0a1cdc2ff1566c449d3b83
Fabio Mattei <burattino@gmail.com>
2012-10-27 01:31:16 +0200

    realining code

--- admin/languages/pt.php
+++ admin/languages/pt.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-    Copyright (C) 2009-2010 Sofia Laureano Schelten
+    Copyright (C) 2009-2012 Sofia Laureano Schelten
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- admin/view/publisher/categories.php
+++ admin/view/publisher/categories.php
@@ -1,7 +1,7 @@
 <?
 
 /*
-    Copyright (C) 2009-2010  Fabio Mattei <burattino@gmail.com>
+    Copyright (C) 2009-2012  Fabio Mattei <burattino@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
